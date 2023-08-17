861aace592b47fc79820af755ffd68843432d76b
TACULMA2 <marigold.taculog@oocl.com>
2023-08-17 20:54:04 +0800

    fix: fix logic, added turn_right command given O O West resulting to 0 0 North. Test success

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -34,6 +34,8 @@ public class MarsRover {
                 location.setDirection(Direction.EAST);
             } else if (location.getDirection() == Direction.EAST) {
                 location.setDirection(Direction.SOUTH);
+            } else if (location.getDirection() == Direction.WEST) {
+                location.setDirection(Direction.NORTH);
             }
         }
 
